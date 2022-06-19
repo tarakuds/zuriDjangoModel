545f8e32d70bea97bce8ef4be59f2f59bcaa70f1
tarakuds <taradebby@gmail.com>
2022-06-19 19:04:50 +0100

    modified app

--- I4G0081907VP/blog/models.py
+++ I4G0081907VP/blog/models.py
@@ -1,16 +1,10 @@
 from django.db import models
-
-# Create your models here.
-# class author (models.Model):
-#     name = models.get_user_model()
-
-    # def __str__(self) -> str:
-    #     return self.name
+from django.contrib.auth import get_user_model
 
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     text = models.TextField()
-    # Author = models.ForeignKey(author, on_delete=models.CASCADE)
+    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     Author = models.TextField()
     Created_date = models.DateField()
     Published_date = models.DateField() 
